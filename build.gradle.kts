09cbf2b4a4adf1d0a4c0893ba5157032a7654811
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-01-27 02:52:37 +0900

    build: Add a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 group = "io.github.rokuosan"
@@ -11,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.github.ajalt.clikt:clikt:4.2.2")
+    implementation("com.charleskorn.kaml:kaml:0.57.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
