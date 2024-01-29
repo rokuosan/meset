055192235d65d07366c5dd57cdeff15b79e3c9a8
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-01-29 17:27:50 +0900

    fix: Change data structure

--- src/main/kotlin/io/github/rokuosan/meset/processor/Processor.kt
+++ src/main/kotlin/io/github/rokuosan/meset/processor/Processor.kt
@@ -3,8 +3,8 @@ package io.github.rokuosan.meset.processor
 import io.github.rokuosan.meset.schema.Task
 
 interface Processor {
-    val parents: List<Processor>
-    val children: List<Processor>
+    val parents: MutableSet<Processor>
+    val children: MutableSet<Processor>
     val task: Task
 
     fun run()
